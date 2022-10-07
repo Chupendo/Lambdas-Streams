0f4f3d815e85ffb829c05f63b2e38409461cb1c7
Andrés Ruiz Peñuela <andres.rpenuela@babelgroup.com>
2022-10-07 15:06:37 +0200

    ejericios de operaciones intermedias/finales de lambda

---
+++ Colecciones/src/lambdas/secction/five/operations/OperAverage.java
@@ -0,0 +1,23 @@
+package lambdas.secction.five.operations;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OperAverage {
+
+	public static void main(String[] args) {
+		// Suma de los numeros al curado de los numeros pares
+		List<Integer> lNumeros = Arrays.asList(1, 2, 3, 4, 5, 2, 3, 4);
+
+		System.out.print("Lista Input: {");
+		lNumeros.forEach(num -> {
+			System.out.print(" " + num);
+		});
+		System.out.println(" }");
+		
+		System.out.println("Funcion final average");
+		Double result = lNumeros.stream().mapToDouble(elem->elem).average().getAsDouble();
+		System.out.println("promedio: "+result);
+	}
+
+}
